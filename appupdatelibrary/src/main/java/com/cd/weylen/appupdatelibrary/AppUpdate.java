e13420394e3b65951ba616a262486cfc51288119
weylen <351531626@qq.com>
2016-11-03 15:41:49 +0800

    修改安装完成后没有提示打开页面

--- appupdatelibrary/src/main/java/com/cd/weylen/appupdatelibrary/AppUpdate.java
+++ appupdatelibrary/src/main/java/com/cd/weylen/appupdatelibrary/AppUpdate.java
@@ -148,6 +148,7 @@ public class AppUpdate {
                 // 安装apk
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setDataAndType(Uri.fromFile(new File(path)), "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
             }
         };
